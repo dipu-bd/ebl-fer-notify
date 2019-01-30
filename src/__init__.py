8c350a59333000256a7d67f948eda1efaed8a2a9
Sudipto Chandra Dipu <dipu.sudipta@gmail.com>
2019-01-30 18:00:55 +0600

    Fix import

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from dotenv import load_dotenv
-from .crawler import download_pdf
+from .crawler import download_latest
 from .reader import parse_pdf
 from .storage import sync_result
 from .notify import notify_result
@@ -8,7 +8,7 @@ from .notify import notify_result
 
 def main():
     load_dotenv()
-    pdf_file = download_pdf()
+    pdf_file = download_latest()
     result = parse_pdf(pdf_file)
     all_results = sync_result(result)
     notify_result(all_results)
