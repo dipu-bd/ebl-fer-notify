8505450b376429c8876cd4d8284e2c44c8b04c5f
Sudipto Chandra Dipu <dipu.sudipta@gmail.com>
2019-01-30 15:04:46 +0600

    Add crawler for fer pdf file

---
+++ src/crawler.py
@@ -0,0 +1,29 @@
+import os
+from datetime import date
+import requests
+from bs4 import BeautifulSoup
+
+website = 'https://www.ebl.com.bd/home/Foreign_Exchange_Rates'
+pdf_folder = os.path.join('data', 'ebl')
+
+def download_latest():
+    response = requests.get(website)
+    soup = BeautifulSoup(response.text, 'lxml')
+    href = soup.select('td.bodytext a.menu')[0]['href']
+
+    response = requests.get(href)
+    pdf_data = response.content
+
+    today = date.today().isoformat()
+    os.makedirs(pdf_folder, exist_ok=True)
+    filename = os.path.join(pdf_folder, '%s.pdf' % today)
+    with open(filename, 'wb') as fp:
+        fp.write(response.content)
+    # end with
+
+    return filename
+# end def
+
+if __name__ == "__main__":
+    download_latest()
+# end if
